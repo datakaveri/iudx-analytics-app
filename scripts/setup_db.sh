2b821b7194ea768699a3f4fc37232c260b5a6446
Sahil Mishra <sahilmishra0012@outlook.com>
2021-07-23 15:25:52 +0530

    Changed create volume command syntax

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -17,24 +17,21 @@ fi
 docker network create analytics-net
 
 (cd ../setup/druid/ \
-    && docker-compose -f docker-compose-create.yml create)
+    && docker-compose -f docker-compose-create.yml up --no-start)
 
 (cd ../setup/druid/ \
     && docker-compose up -d)
 
 
-while true; 
+while true;
 curl -XGET http://localhost:8888
 retval=$?
-do 
+do
   if [ $retval -eq 0 ] ; then
     (cd ../setup/apps/ \
         && docker-compose up druidinit)
     break
   fi
   echo "Druid not up yet"
-  sleep 1; 
+  sleep 1;
 done
-
-
-
